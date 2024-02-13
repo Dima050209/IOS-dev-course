799a73914afe0fa3fa37d1b6e24c621c91487b58
Dmytro Kharchenko <dmytriy4565@gmail.com>
2024-02-13 18:24:53 +0100

    added text change

--- IOS_Course/ViewController.swift
+++ IOS_Course/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var authorName: UILabel!
+    @IBOutlet weak var domain: UILabel!
+    @IBOutlet weak var postTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         PostNetworkService.shared.fetchRedditAPIWithDataTask() { res in
-            print(res!)
+            DispatchQueue.main.async {
+                if let res = res {
+                    self.authorName.text = res.data.children[0].data.author
+                    self.domain.text = res.data.children[0].data.domain
+                    self.postTitle.text = res.data.children[0].data.title
+                } else {
+                    // Обробка випадку, коли res є нульовим
+                }
+            }
         }
         
     }
