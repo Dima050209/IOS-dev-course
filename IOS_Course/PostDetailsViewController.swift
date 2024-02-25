896b3c329e2d49b6a29e1d276def5700ca7c77fd
Dmytro Kharchenko <dmytriy4565@gmail.com>
2024-02-25 20:36:19 +0100

    added post details

--- IOS_Course/PostDetailsViewController.swift
+++ IOS_Course/PostDetailsViewController.swift
@@ -33,30 +33,42 @@ class PostDetailsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     func config(with post:Child) {
-        var myPost = MyPost(redditPost: post)
-       //        print(myPost)
-               DispatchQueue.main.async {
-                   guard let name = self.authorName else {
-                       return
-                   }
-                   self.authorName.text = myPost.author
-       //            self.timePassed.text = myPost.timePassed
-       //            self.postTitle.text = myPost.title
-       //            self.domain.text = myPost.domain
-       //            // temporary
-       //            self.savedBtn.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
-       //            if let image = myPost.image {
-       //                DispatchQueue.global().async { [weak self] in
-       //                    if let data = try? Data(contentsOf: image) {
-       //                        DispatchQueue.main.async {
-       //                            self?.img?.image = UIImage(data: data)
-       //                        }
-       //                    }
-       //                }
-       //            }
-       //            self.commentsBtn.setTitle(String(myPost.rating), for: .normal)
-       //            self.ratingBtn.setTitle(String(myPost.comments), for: .normal)
-               }
+        let myPost = MyPost(redditPost: post)
+        DispatchQueue.main.async {
+            guard self.authorName != nil else { return }
+            self.authorName.text = myPost.author
+                   
+            guard self.timePeriod != nil else { return }
+            self.timePeriod.text = myPost.timePassed
+                   
+            guard self.postTitle != nil else { return }
+            self.postTitle.text = myPost.title
+                   
+            guard self.domain != nil else { return }
+            self.domain.text = myPost.domain
+    
+            guard self.savedBtn != nil else { return }
+            if myPost.saved {
+                self.savedBtn.setImage(UIImage(systemName: "bookmark.fill"), for: .normal)
+            } else {
+                self.savedBtn.setImage(UIImage(systemName: "bookmark"), for: .normal)
+            }
+                   
+            if let image = myPost.image {
+                DispatchQueue.global().async { [weak self] in
+                    if let data = try? Data(contentsOf: image) {
+                        DispatchQueue.main.async {
+                            self?.img?.image = UIImage(data: data)
+                        }
+                    }
+                }
+            }
+            guard self.commentsBtn != nil else { return }
+            self.commentsBtn.setTitle(String(myPost.rating), for: .normal)
+                   
+            guard self.ratingBtn != nil else { return }
+            self.ratingBtn.setTitle(String(myPost.comments), for: .normal)
+        }
     }
     
 
